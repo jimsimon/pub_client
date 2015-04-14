d2d6f8830f24958d557cd42ceb07d79dab158f5c
Jim Simon <jim.j.simon@gmail.com>
2015-04-14 00:14:47 -0400

    Implemented all current api calls (still need more tests) and switched to actual objects instead of maps

--- lib/pub_client.dart
+++ lib/pub_client.dart
@@ -1,9 +1,8 @@
 library pub_client;
 
-//import "dart:io";
 import "package:http/http.dart";
+import "package:jsonx/jsonx.dart";
 import "dart:async";
-import "dart:convert";
 
 class PubClient {
 
@@ -26,25 +25,101 @@ class PubClient {
     return url;
   }
 
-  Future<List<Map>> getAllPackages() async {
+  Future<List<Package>> getAllPackages() async {
     var packages = [];
     var currentPage = 1;
     var totalPages = 1;
     while (currentPage <= totalPages) {
-      var response = await getPageOfPackages(currentPage);
-      packages.addAll(response["packages"]);
-      totalPages = response["pages"];
+      Page page = await getPageOfPackages(currentPage);
+      packages.addAll(page.packages);
+      totalPages = page.pages;
       currentPage++;
     }
-    print("${packages.length} packages found");
     return packages;
   }
 
-  Future<Map> getPageOfPackages(pageNumber) async {
+  Future<Page> getPageOfPackages(pageNumber) async {
     var url = "$baseApiUrl/packages?page=$pageNumber";
-    print("Requesting data from $url");
     Response response = await client.get(url, headers: _HEADERS);
-    Map jsonResponse = JSON.decode(response.body);
-    return jsonResponse;
+    if (response.statusCode >= 300) {
+      throw new HttpException(response.statusCode, response.body);
+    }
+    Page page = decode(response.body, type: Page);
+    return page;
   }
+
+
+  Future<FullPackage> getPackage(String name) async {
+    var url = "$baseApiUrl/packages/$name";
+    Response response = await client.get(url, headers: _HEADERS);
+//    if (response.statusCode >= 300) {
+//      throw new HttpException(response.statusCode, response.body);
+//    }
+    FullPackage package = decode(response.body, type: FullPackage);
+    return package;
+  }
+}
+
+class Page {
+  String next_url;
+  List<Package> packages;
+  String prev_url;
+  int pages;
+}
+
+class Package {
+  String name;
+  String url;
+  String uploaders_url;
+  String new_version_url;
+  String version_url;
+  Version latest;
+}
+
+class FullPackage extends Package {
+  DateTime created;
+  int downloads;
+  List<String> uploaders;
+  List<Version> versions;
+}
+
+class Version {
+  Pubspec pubspec;
+  String url;
+  String archive_url;
+  String version;
+  String new_dartdoc_url;
+  String package_url;
+}
+
+class Pubspec {
+  Environment environment;
+  String version;
+  String description;
+  String author;
+  Map<String, String> dev_dependencies;
+  Map<String, String> dependencies;
+  String homepage;
+  String name;
+}
+
+class Environment {
+  String sdk;
+}
+
+class HttpException implements Exception {
+
+  int status;
+  String message;
+  HttpException(int this.status, [String this.message]);
+
+  String toString() {
+    String stringRepresentation =  "$status";
+    if (message != null) {
+      stringRepresentation += ": $message";
+    }
+    return stringRepresentation;
+  }
+
+
 }
--- test/pub_client_test.dart
+++ test/pub_client_test.dart
@@ -4,19 +4,55 @@ import "package:http/testing.dart";
 import "package:http/http.dart";
 
 main() {
-  PubClient client;
+
+  MockClient mockClient = new MockClient((Request request) {
+    if (request.url.path == "/api/packages") {
+      if (request.url.queryParameters["page"] == "1") {
+        var responseText = '''
+            {"next_url": "https://pub.dartlang.org/api/packages?page=2", "packages": [
+              {"name": "abc123", "url": "https://pub.dartlang.org/api/packages/abc123", "uploaders_url": "https://pub.dartlang.org/api/packages/abc123/uploaders", "new_version_url": "https://pub.dartlang.org/api/packages/abc123/versions/new", "version_url": "https://pub.dartlang.org/api/packages/abc123/versions/{version}", "latest": {"pubspec": {"environment": {"sdk": ">=1.0.0 <2.0.0"}, "version": "1.3.5", "description": "abc123 library", "author": "Abc Team <abc@123.org>", "dev_dependencies": {"unittest": ">=0.9.0 <0.12.0"}, "homepage": "http://github.com/dart-lang/abc123", "name": "abc123"}, "url": "https://pub.dartlang.org/api/packages/abc123/versions/1.3.5", "archive_url": "https://pub.dartlang.org/packages/abc123/versions/1.3.5.tar.gz", "version": "1.3.5", "new_dartdoc_url": "https://pub.dartlang.org/api/packages/abc123/versions/1.3.5/new_dartdoc", "package_url": "https://pub.dartlang.org/api/packages/abc123"}},
+              {"name": "cde456", "url": "https://pub.dartlang.org/api/packages/cde456", "uploaders_url": "https://pub.dartlang.org/api/packages/cde456/uploaders", "new_version_url": "https://pub.dartlang.org/api/packages/cde456/versions/new", "version_url": "https://pub.dartlang.org/api/packages/cde456/versions/{version}", "latest": {"pubspec": {"environment": {"sdk": ">=1.0.0 <2.0.0"}, "version": "1.3.5", "description": "cde456 library", "author": "Abc Team <abc@123.org>", "dev_dependencies": {"unittest": ">=0.9.0 <0.12.0"}, "homepage": "http://github.com/dart-lang/cde456", "name": "cde456"}, "url": "https://pub.dartlang.org/api/packages/cde456/versions/1.3.5", "archive_url": "https://pub.dartlang.org/packages/cde456/versions/1.3.5.tar.gz", "version": "1.3.5", "new_dartdoc_url": "https://pub.dartlang.org/api/packages/cde456/versions/1.3.5/new_dartdoc", "package_url": "https://pub.dartlang.org/api/packages/cde456"}}
+            ], "prev_url": null, "pages": 1}
+          ''';
+        return new Response(responseText, 200);
+      } else {
+        return new Response("Not found", 404);
+      }
+    } else if (request.url.path == "/api/packages/abc123") {
+      return new Response('{"name": "abc123", "created": "2013-07-16T00:21:54.360590", "url": "https://pub.dartlang.org/api/packages/abc123", "uploaders_url": "https://pub.dartlang.org/api/packages/abc123/uploaders", "new_version_url": "https://pub.dartlang.org/api/packages/abc123/versions/new", "version_url": "https://pub.dartlang.org/api/packages/abc123/versions/{version}", "latest": {"pubspec": {"environment": {"sdk": ">=1.0.0 <2.0.0"}, "version": "1.3.5", "description": "abc123 library", "author": "Abc Team <abc@123.org>", "dev_dependencies": {"unittest": ">=0.9.0 <0.12.0"}, "homepage": "http://github.com/dart-lang/abc123", "name": "abc123"}, "url": "https://pub.dartlang.org/api/packages/abc123/versions/1.3.5", "archive_url": "https://pub.dartlang.org/packages/abc123/versions/1.3.5.tar.gz", "version": "1.3.5", "new_dartdoc_url": "https://pub.dartlang.org/api/packages/abc123/versions/1.3.5/new_dartdoc", "package_url": "https://pub.dartlang.org/api/packages/abc123"}}', 200);
+    }
+    return new Response('', 404);
+  });
+
+  PubClient client = new PubClient(client: mockClient);;
 
   group("client", () {
     test("can retrieve a page of packages when a valid page number is specified", () async {
-      MockClient mockClient = new MockClient((Request request) {
-        if (request.url.path == "/api/packages") {
-          return new Response("{}", 200);
-        }
-        return new Response("", 404);
-      });
-      client = new PubClient(client: mockClient);
-      Map packages = await client.getPageOfPackages(1);
-      expect(packages["name"], equals("test package"));
+      Page page = await client.getPageOfPackages(1);
+      expect(page.next_url, "https://pub.dartlang.org/api/packages?page=2");
+      expect(page.prev_url, isNull);
+      expect(page.pages, 1);
+      expect(page.packages.length, 2);
+      expect(page.packages[0].name, "abc123");
+      expect(page.packages[1].name, "cde456");
+    });
+
+    test("throws an exception for invalid page number when retrieving a page of packages", () async {
+      expect(client.getPageOfPackages(2), throwsA(new isInstanceOf<HttpException>()));
+    });
+
+    test("can retrieve all packages", () async {
+      List<Package> packages = await client.getAllPackages();
+      expect(packages[0].name, "abc123");
+      expect(packages[0].latest.pubspec.dev_dependencies.length, 1);
+      expect(packages[1].name, "cde456");
+      expect(packages[1].latest.pubspec.dev_dependencies.length, 1);
+    });
+
+    test("can retrieve a specific package", () async {
+      FullPackage package = await client.getPackage("abc123");
+      var now = new DateTime.now();
+      expect(now.isAfter(package.created), isTrue);
     });
   });
 }
