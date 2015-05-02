0e501039651b5bb3150f55ce1ee325934f5f3e23
Jim Simon <jim.j.simon@gmail.com>
2015-05-01 23:56:56 -0700

    Switched from unittest package to test package

--- test/pub_client_test.dart
+++ test/pub_client_test.dart
@@ -1,4 +1,4 @@
-import "package:unittest/unittest.dart";
+import "package:test/test.dart";
 import "package:pub_client/pub_client.dart";
 import "package:http/testing.dart";
 import "package:http/http.dart";
